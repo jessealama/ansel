e37de1c83bb2856d603a6c1b6833307b3e4f030c
Jesse Alama <alama@stanford.edu>
2009-04-27 14:19:12 -0700

    Track seconds and use events (specifically, SCREENSHOT_SUCCEEDED)
    * Ansel.lua: Two major changes: keep track of seconds, and use events
    
    1. Currently, GetGameTime returns the server time in hours and
    minutes.  For our purposes, that's not enough: since screenshot file
    names contain the hour, minute, and second when they were taken, and
    since we need to know the screenshot file name, we need to know the
    second when the screenshot is taken.  For that, I've adoped a hack
    that I found on the WowWiki:
    
      http://www.wowwiki.com/GameTime:Get
    
    This module keeps track of the second.  Its accuracy is, perhaps
    surprisingly, limited by the current framerate.  It seems to work well
    enough for our purposes, though.
    
    2. We're now tracking the SCREENSHOT_SUCCESSFUL event.
    
    I found to my surprise that the Screenshot() function apparently runs
    asynchronously.  The sequence of functions
    
      UIParent:Hide()
      Screenshot()
      UIParent:Show()
    
    intuitively says: hide the UI (equivalent to hitting alt-z in WoW),
    then take a screenshot, then reveal the UI.  The screenehots generated
    in this way, however, do contain the UI.  What's apparently happening
    is that the Screenshot() function completes its exeuction *before* the
    screen is actually captured and the file generated.  Control is passed
    to UIParent:Show() before the screen gets captured.  One is tempted to
    insert useless idle computation between the Screenshot() and the
    UIParent:Show(), but this doesn't feel right.  (And there doesn't
    appear to be a sleep or wait or idle function, anyway.)
    
    A solution was suggested on #wowuidev on irc.freenode.net: use events.
    Specifically, upon loading Ansel now tracks the SCREENSHOT_SUCCEEDED
    event.  AnselTakeSingleShot() now essentially records the time when it
    is called, hides the UI if that is requested, and takes a screenshot.
    The function Ansel_OnEvent now actually does something: when it
    receives the event SCREENSHOT_SUCCEEDED, it reveals the UI (if it was
    hidden when the screenshot was taken) and computes the time and other
    features of the screenshot.

--- Ansel.lua
+++ Ansel.lua
@@ -4,18 +4,105 @@ Ansel: A World of Warcraft screenshot addon to rule them all.
 
 --]]
 
+Ansel_GameTime = {
+
+  -----------------------------------------------------------
+  -- function Ansel_GameTime:Get()
+  --
+  -- Return game time as (h,m,s) where s has 3 decimals of
+  -- precision (though it's only likely to be precise down
+  -- to ~20th of seconds since we're dependent on frame
+  -- refreshrate).
+  --
+  -- During the first minute of play, the seconds will
+  -- consistenly be "00", since we haven't observed any
+  -- minute changes yet.
+  --
+  --
+
+  Get = function(self)
+  	if(self.LastMinuteTimer == nil) then
+  		local h,m = GetGameTime();
+  		return h,m,0;
+  	end
+  	local s = GetTime() - self.LastMinuteTimer;
+  	if(s>59.999) then
+  		s=59.999;
+  	end
+  	return self.LastGameHour, self.LastGameMinute, s;
+  end,
+
+
+  -----------------------------------------------------------
+  -- function Ansel_GameTime:OnUpdate()
+  --
+  -- Called by: Private frame <OnUpdate> handler
+  --
+  -- Construct high precision server time by polling for
+  -- server minute changes and remembering GetTime() when it
+  -- last did
+  --
+
+  OnUpdate = function(self)
+  	local h,m = GetGameTime();
+  	if(self.LastGameMinute == nil) then
+  		self.LastGameHour = h;
+  		self.LastGameMinute = m;
+  		return;
+  	end
+  	if(self.LastGameMinute == m) then
+  		return;
+  	end
+  	self.LastGameHour = h;
+  	self.LastGameMinute = m;
+  	self.LastMinuteTimer = GetTime();
+  end,
+
+  -----------------------------------------------------------
+  -- function Ansel_GameTime:Initialize()
+  --
+  -- Create frame to pulse OnUpdate() for us
+  --
+
+  Initialize = function(self)
+  	self.Frame = CreateFrame("Frame");
+  	self.Frame:SetScript("OnUpdate", function() self:OnUpdate(); end);
+  end
+}
+
+Ansel_GameTime:Initialize();
+
 -- For the keybindings
 BINDING_HEADER_ANSEL = "Ansel"
+
+-- Variables
+
+local current_hour, current_minute, current_second;
+local uihidden = false;
+
+-- Screenshots
 
 function purpleChatMessage (s)
    DEFAULT_CHAT_FRAME:AddMessage (s, 1.0, 0.0, 1.0);
 end
 
 function Ansel_OnLoad ()
-   purpleChatMessage("Ansel now loaded -- take screenshots like a pro!");
+   this:RegisterEvent ("SCREENSHOT_SUCCEEDED");
+   purpleChatMessage("Ansel is now loaded -- take screenshots like a pro!");
 end
 
 function Ansel_OnEvent (event)
+   if (event == "SCREENSHOT_SUCCEEDED") then
+      local zone     = GetRealZoneText();
+      local subzone  = GetSubZoneText();
+      local h,m,s    = Ansel_GameTime:Get();
+      local x,y      = GetPlayerMapPosition("player");
+      if (uihidden) then
+	 UIParent:Show ();
+	 uihidden = true;
+      end
+      purpleChatMessage ("Screenshot taken in subzone " .. subzone .. " of " .. zone .. " at gametime " .. h .. ":" .. m .. ":" .. s .. " in location (" .. x .. "," .. y ..")");
+   end
 end
 
 function AnselRapidFireScreenshot ()
@@ -25,11 +112,10 @@ function AnselRapidFireScreenshot ()
 end
 
 function AnselSingleShot ()
-   local zone    = GetRealZoneText();
-   local subzone = GetSubZoneText();
-   local time    = GetGameTime();
-   local x, y    = GetPlayerMapPosition("player");
-   -- now ignore these data
+   if (IsShiftKeyDown ()) then
+      uihidden = true;
+      UIParent:Hide ();      
+   end
+   current_hour, current_minute, current_second = Ansel_GameTime:Get ();
    Screenshot();
-   -- purpleChatMessage ("Screenshot taken in subzone " .. subzone .. " of " .. zone " at gametime " .. time .. "in location \(" x .. "," .. y .."\)");
 end
