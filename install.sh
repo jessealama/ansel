b95b3ccd719d0cde5d7cb94b2d8895d02fbfa3b6
Jesse Alama <alama@stanford.edu>
2009-04-26 17:08:54 -0700

    Begin work on an installation script
    * install.sh: Initial commit.

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+APPLICATIONS=/Applications
+WOW=$APPLICATIONS/World\ of\ Warcraft
+ADDON_DIR=$WOW/Interface/AddOns
+ANSEL_DIR=$ADDON_DIR/Ansel
+ANSEL_FILES='Ansel.lua Ansel.toc Bindings.xml'
+
+if [[ ! -d $ANSEL_DIR ]]; then
+    echo Making Ansel directory in $ADDON_DIR;
+    mkdir "$ANSEL_DIR";
+fi
+
+echo -n Installing ansel...;
+
+for i in $ANSEL_FILES; do
+    cp $i "$ANSEL_DIR";
+done
+
+echo done
