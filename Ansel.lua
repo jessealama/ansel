7bdb95cdfbc42fbd4522189a9f714545b37c2b4d
Jesse Alama <alama@stanford.edu>
2009-04-26 19:44:37 -0700

    Update keybindings info, make the load message spicier, and fix a typo
    * Ansel.lua: Minor additions and modifications.
    
    BINDING_HEADER_ANSEL: New variable, used to put a header into the
      keybindings area.
    
    Ansel_OnLoad: Spicier post-load message.
    
    AnselRapidFireScreenshot: It's "Screenshot()", not "ScreenShot()".

--- Ansel.lua
+++ Ansel.lua
@@ -1,25 +1,27 @@
---[
+--[[
 
 Ansel: A World of Warcraft screenshot addon to rule them all.
 
---]
+--]]
+
+-- For the keybindings
+BINDING_HEADER_ANSEL = "Ansel"
 
 function purpleChatMessage (s)
-   DEFAULT_CHAT_FRAME:AddMessage(s, 1.0, 0.0, 1.0, 1.0);
+   DEFAULT_CHAT_FRAME:AddMessage (s, 1.0, 0.0, 1.0);
 end
 
 function Ansel_OnLoad ()
-   purpleChatMessage("Ansel: Take screenshots like a pro!");
+   purpleChatMessage("Ansel now loaded -- take screenshots like a pro!");
 end
 
 function Ansel_OnEvent (event)
-   -- I don't know what to do here
 end
 
 function AnselRapidFireScreenshot ()
    purpleChatMessage ("Rapid-fire screenshots not yet enabled;");
    purpleChatMessage ("Taking a single screenshot instead.")
-   ScreenShot ();
+   Screenshot ();
 end
 
 function AnselSingleShot ()
@@ -29,4 +31,5 @@ function AnselSingleShot ()
    local x, y    = GetPlayerMapPosition("player");
    -- now ignore these data
    Screenshot();
+   -- purpleChatMessage ("Screenshot taken in subzone " .. subzone .. " of " .. zone " at gametime " .. time .. "in location \(" x .. "," .. y .."\)");
 end
