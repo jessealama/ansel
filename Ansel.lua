26b1dcc415ae30c7080b4f2a3130edeedc11ca55
Jesse Alama <alama@stanford.edu>
2009-05-02 14:36:54 -0500

    Maintain a screenshot database
    * Ansel.lua: Add support for a screenshot database
    
    AnselPhotoDB: new variable
    
    purpleChatMessage: renamed to purpleAnselChatMessage
    
    purpleAnselChatMessage: new function
     prepend "[Ansel]" to all messages
    
    Ansel_OnLoad: watch for SCREENSHOT_FAILED and VARIABLES_LOADED
    
    Ansel_OnEvent: add to AnselPhotoDB, and respond to events that we now watch
    
    AnselRapidFireScreenshot: Use purpleAnselChatMessage.
    
    AnselSingleShot: close all windows before hiding the UI
    
    * Ansel.toc: Declare AnselPhotoDB to be persistent

--- Ansel.lua
+++ Ansel.lua
@@ -75,20 +75,30 @@ Ansel_GameTime:Initialize();
 -- For the keybindings
 BINDING_HEADER_ANSEL = "Ansel"
 
--- Variables
-
+-- Keep track of the hour, minute, and second just before a screenshot
+-- is taken.  We store this information in the photo database, along
+-- with the the hour, minute, and second immediately after a
+-- screenshot is taken.  We use these data to figure out what file in
+-- the Screenshots directory corresponds to the the screenshot we
+-- took.
 local current_hour, current_minute, current_second;
+
+-- We keep track of whether the UI was hidden before we took the last
+-- screenshot.
 local uihidden = false;
 
--- Screenshots
+-- Photo database
+AnselPhotoDB = {};
 
-function purpleChatMessage (s)
-   DEFAULT_CHAT_FRAME:AddMessage (s, 1.0, 0.0, 1.0);
+function purpleAnselChatMessage (s)
+   DEFAULT_CHAT_FRAME:AddMessage ("[Ansel] " .. s, 1.0, 0.0, 1.0);
 end
 
 function Ansel_OnLoad ()
    this:RegisterEvent ("SCREENSHOT_SUCCEEDED");
-   purpleChatMessage("Ansel is now loaded -- take screenshots like a pro!");
+   this:RegisterEvent ("SCREENSHOT_FAILED");
+   this:RegisterEvent ("VARIABLES_LOADED");
+   purpleAnselChatMessage("Take screenshots like a pro!");
 end
 
 function Ansel_OnEvent (event)
@@ -101,19 +111,37 @@ function Ansel_OnEvent (event)
 	 UIParent:Show ();
 	 uihidden = false;
       end
-      purpleChatMessage ("Screenshot taken in subzone " .. subzone .. " of " .. zone .. " at gametime " .. h .. ":" .. m .. ":" .. s .. " in location (" .. x .. "," .. y ..")");
+      local record = { ["preshot hour"] = current_hour, 
+		       ["preshot minute"] = current_minute, 
+		       ["preshot second"] = current_second,
+		       ["postshot hour"] = h,
+		       ["postshot minute"] = m,
+		       ["postshot second"] = s,
+		       ["zone"] = zone,
+		       ["subzone"] = subzone,
+		       ["x"] = x,
+		       ["y"] = y };
+      table.insert(AnselPhotoDB,record);
+      purpleAnselChatMessage ("Screenshot taken in subzone " .. subzone .. " of " .. zone .. " at gametime " .. h .. ":" .. m .. ":" .. s .. " in location (" .. x .. "," .. y ..")");
+   elseif (event == "SCREENSHOT_FAILED") then
+      purpleAnselChatMessage ("Screenshot failed -- sorry!");
+   elseif (event == "VARIABLES_LOADED") then
+      purpleAnselChatMessage ("Screenshot database loaded");
+   else
+      -- do nothing
    end
 end
 
 function AnselRapidFireScreenshot ()
-   purpleChatMessage ("Rapid-fire screenshots not yet enabled;");
-   purpleChatMessage ("Taking a single screenshot instead.")
+   purpleAnselChatMessage ("Rapid-fire screenshots not yet enabled;");
+   purpleAnselChatMessage ("Taking a single screenshot instead.")
    Screenshot ();
 end
 
 function AnselSingleShot ()
    if (IsShiftKeyDown ()) then
       uihidden = true;
+      CloseAllWindows();
       UIParent:Hide ();      
    end
    current_hour, current_minute, current_second = Ansel_GameTime:Get ();
